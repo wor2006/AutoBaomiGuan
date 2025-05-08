30a5c6351181307ca95d151cbdbca129278e8745
wor2006 <2076587315@qq.com>
2025-05-08 21:28:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 """
 
 # 登录信息
-loginName = "xxxx" # 保密观账号
-passWord = "xxxx" # 保密观密码
+loginName = "15244910325" # 保密观账号
+passWord = "zhangyi2006" # 保密观密码
 
 # 课程ID
 course_packet_id = "21c7d935-dd53-49d2-a95f-dc0f3e14ced7"  # 2025年度保密教育线上培训 课程参数
